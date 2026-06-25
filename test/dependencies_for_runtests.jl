82069838d744d9b93378047da4623ab4f463bcd0
Jago Strong-Wright <jagoosw@protonmail.com>
2026-06-25 21:42:51 +0100

    catch GPUs/CUDA failing

--- test/dependencies_for_runtests.jl
+++ test/dependencies_for_runtests.jl
@@ -2,4 +2,10 @@ using OceanBioME, Test, CUDA, Oceananigans, JLD2, Oceananigans.Units, Documenter
 
 using Oceananigans.Fields: ConstantField
 
-architecture = CUDA.functional() ? GPU() : CPU()
+if get(ENV, "CUDA_VISIBLE_DEVICES", nothing) == "-1"
+    architecture = CPU()
+elseif CUDA.functional()
+    architecture = GPU()
+else
+    error("CUDA is not functional but CUDA_VISIBLE_DEVICES is not set to -1. GPU tests cannot run without a working GPU.")
+end
